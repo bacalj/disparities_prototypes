260a2776160e5a9608de4e9096000db11d201641
Joseph Bacal <bacalj@me.com>
2014-08-28 19:31:59 -0400

    working template

--- script.js
+++ script.js
@@ -7,92 +7,33 @@
 
 $(document).ready(function() {
 
-	var i = 0;
+	var chart = new Highcharts.Chart({
 
-	var myData = 
-	[
-		{
-			x:2008, low:70, high:80, pops:"Bears"
-		},
+    chart: {
+        renderTo: 'chart_container'
+    },
 
-		{
-			x:2009, low:60, high:87, pops:"Geese"
-		},
+    tooltip: {
+        formatter: function () {
+            return 'special nice:  <br>' + this.point.foo + '<br>' + this.point.blug;
+        }
+    },
 
-		{
-			x:2010, low:70, high:77, pops:"Snakes"
-		},
-
-		{
-			x:2011, low:72, high:87, pops:"Trolls"
-		},
-
-		{
-			x:2012, low:60, high:67, pops:"Fish"
-		}
-	];
-
-	$('#chart_container').highcharts({
-		chart: {
-			type: 'columnrange',
-			inverted: false
-		},
-
-		title: {
-			text: 'Summary Measures of Health Disparities'
-		},
-
-		subtitle: {
-			text: 'by Race/Ethnicity'
-		},
-
-		yAxis: {
-			min: 50,
-			max: 100,
-			title: {
-				text: '% (percent)'
-			}
-		},
-
-		tooltip: {
-			valueSuffix: '%'
-		},
-
-		plotOptions: {
-
-			series: {
-				color:'black',
-				pointWidth: 2
-			}, 
-
-			columnrange: {
-				dataLabels: {
-					enabled: true,
-					align:'center',
-					formatter: function () {
-						var label = this.point.pops + ' ' + this.y;
-						console.log(this);
-						return label;
-					}
-					/*formatter: function() {
-    				var lowPoint = getLow(this.point.low);
-    				var highPoint = getHigh(this.point.high);
-    				return this.y;
-					}*/
-				},
-			}
-		},
-
-		legend: {
-			enabled: false
-		},
-
-		series: 
-		[
-			{
-				name: 'Disparities',
-				data: myData
-			}
-		]
+    series: [{
+        name: 'Ya',
+        data: [{
+            y: 3,
+            foo: 'gorf gorf',
+            blug:'snah'
+        }, {
+            y: 7,
+            foo: 'fligiity',
+            blug: 'gork'
+        }, {
+            y: 1,
+            foo: 'shib',
+            blug: 'kroop'
+        }]
+    }]
 	});
 });
