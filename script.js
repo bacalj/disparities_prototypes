a2593014061ca5539e48b79d42c8a8db6121b73a
Joseph Bacal <bacalj@me.com>
2014-08-28 23:31:32 -0400

    cleaned up

--- script.js
+++ script.js
@@ -1,98 +1,92 @@
-//each year
-	//data like this
-	// | YEAR | OBJECTIVE | POPULATION | PERCENT |
-	//for each year, find the population with the highest number and plot it where y = %
-	//for each year, find the population with the lowest number and plot it where y =
-	//plot a line from lo to hi
-
 $(document).ready(function() {
 
-var myData = [
+var myData = 
+	[
 
-	    	{
-	        name: 'Best',
-	        data: 
-	        [
-	        	{
-	            y: 87.5,
-	            x: '2008',
-	            population:'White only, not Hispanic or Latino'
-	        	},
+		{
+		  name: 'Best',
+		  data: 
+		  [
+		   	{
+		      y: 87.5,
+		      x: '2008',
+		      population:'White only, not Hispanic or Latino'
+		    },
 
-	        	{
-	            y: 86.8,
-	            x: '2009',
-	            population: 'White only, not Hispanic or Latino'
-	        	},
+		    {
+		      y: 86.8,
+		      x: '2009',
+		      population: 'White only, not Hispanic or Latino'
+		    },
 
-	        	{
-	            y: 86.3,
-	            x: '2010',
-	            population: 'White only, not Hispanic or Latino'
-	        	},
+		    {
+		      y: 86.3,
+		    	x: '2010',
+		      population: 'White only, not Hispanic or Latino'
+		    },
 
-	        	{
-	            y: 87.1,
-	            x: '2011',
-	            population: 'White only, not Hispanic or Latino'
-	        	},
+		    {
+		      y: 87.1,
+		      x: '2011',
+		      population: 'White only, not Hispanic or Latino'
+		    },
 
-	        	{
-	            y: 87.9,
-	            x: '2012',
-	            population: 'Native Hawaiian or Other Pacific Islander'
-	        	}
-	        ]
-	    	}, 
+		    {
+		      y: 87.9,
+		      x: '2012',
+		    	population: 'Native Hawaiian or Other Pacific Islander'
+		    }
+		   ]
+		}, 
 
-	    	{
-	    		name: 'Worst',
-	    		data:
-	    		[
-	    			{
-	    				y: 66.7,
-	            x: '2008',
-	            population:'Hispanic or Latino'
-	    			},
+		{
+		  name: 'Worst',
+		  data:
+		  [
+		    {
+		    	y: 66.7,
+		      x: '2008',
+		      population:'Hispanic or Latino'
+		    },
 
-	    			{
-	    				y: 67.1,
-	            x: '2009',
-	            population:'Hispanic or Latino'
-	    			},
+		    {
+		    	y: 67.1,
+		      x: '2009',
+		      population:'Hispanic or Latino'
+		    },
 
-	    			{
-	    				y: 56.0,
-	            x: '2010',
-	            population:'American Indian or Alaska Native'
-	    			},
+		    {
+		    	y: 56.0,
+		      x: '2010',
+		      population:'American Indian or Alaska Native'
+		    },
 
-	    			{
-	    				y: 65.8,
-	            x: '2011',
-	            population:'American Indian or Alaska Native'
-	    			},
+		    {
+		    	y: 65.8,
+		      x: '2011',
+		      population:'American Indian or Alaska Native'
+		    },
 
-	    			{
-	    				y: 69.6,
-	            x: '2012',
-	            population:'Hispanic or Latino'
-	    			}
-	    		]
-	    	}
-	    ];
+		    {
+		    	y: 69.6,
+		      x: '2012',
+		      population:'Hispanic or Latino'
+		    }
+		  ]
+		}
+	];
 
 
-var chart = new Highcharts.Chart
+	var chart = new Highcharts.Chart
 	(
 		{
 
-	    chart: {
-	        renderTo: 'chart_container'
+	    chart:{
+	      renderTo: 'chart_container'
 	    },
 
 	    plotOptions: {
-				series: {
+				series:{
 					lineWidth:0
 				}
 			}, 
@@ -118,6 +112,6 @@ var chart = new Highcharts.Chart
 	console.log('drilling down, see?');
 	console.log(dataSetBest);
 	console.log(dataSetBest[0]);
-	console.log(dataSetBest[0].population);
+	console.log(dataSetBest[0].x);
 
 });
