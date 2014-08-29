5991f3a26a1c28a4726a1b6f04b9bf2d88e44454
Joseph Bacal <bacalj@me.com>
2014-08-28 23:08:32 -0400

    working w a commment

--- script.js
+++ script.js
@@ -11,104 +11,105 @@ $(document).ready(function() {
 	 then call those in places where hard coded below
 	 you can see that you figured out how to access objects
 	*/
-	var chart = new Highcharts.Chart({
+var chart = new Highcharts.Chart
+	(
+		{
 
-    chart: {
-        renderTo: 'chart_container'
-    },
+	    chart: {
+	        renderTo: 'chart_container'
+	    },
 
-    plotOptions: {
-			series: {
-				lineWidth:0
-			}
-		}, 
+	    plotOptions: {
+				series: {
+					lineWidth:0
+				}
+			}, 
 
-    series: 
-    [
-    	{
-        name: 'Best',
-        data: 
-        [
-        	{
-            y: 87.5,
-            x: '2008',
-            population:'White only, not Hispanic or Latino'
-        	},
+	    series: 
+	    [
+	    	{
+	        name: 'Best',
+	        data: 
+	        [
+	        	{
+	            y: 87.5,
+	            x: '2008',
+	            population:'White only, not Hispanic or Latino'
+	        	},
 
-        	{
-            y: 86.8,
-            x: '2009',
-            population: 'White only, not Hispanic or Latino'
-        	},
+	        	{
+	            y: 86.8,
+	            x: '2009',
+	            population: 'White only, not Hispanic or Latino'
+	        	},
 
-        	{
-            y: 86.3,
-            x: '2010',
-            population: 'White only, not Hispanic or Latino'
-        	},
+	        	{
+	            y: 86.3,
+	            x: '2010',
+	            population: 'White only, not Hispanic or Latino'
+	        	},
 
-        	{
-            y: 87.1,
-            x: '2011',
-            population: 'White only, not Hispanic or Latino'
-        	},
+	        	{
+	            y: 87.1,
+	            x: '2011',
+	            population: 'White only, not Hispanic or Latino'
+	        	},
 
-        	{
-            y: 87.9,
-            x: '2012',
-            population: 'Native Hawaiian or Other Pacific Islander'
-        	}
-        ]
-    	}, 
+	        	{
+	            y: 87.9,
+	            x: '2012',
+	            population: 'Native Hawaiian or Other Pacific Islander'
+	        	}
+	        ]
+	    	}, 
 
-    	{
-    		name: 'Worst',
-    		data:
-    		[
-    			{
-    				y: 66.7,
-            x: '2008',
-            population:'Hispanic or Latino'
-    			},
+	    	{
+	    		name: 'Worst',
+	    		data:
+	    		[
+	    			{
+	    				y: 66.7,
+	            x: '2008',
+	            population:'Hispanic or Latino'
+	    			},
 
-    			{
-    				y: 67.1,
-            x: '2009',
-            population:'Hispanic or Latino'
-    			},
+	    			{
+	    				y: 67.1,
+	            x: '2009',
+	            population:'Hispanic or Latino'
+	    			},
 
-    			{
-    				y: 56.0,
-            x: '2010',
-            population:'American Indian or Alaska Native'
-    			},
+	    			{
+	    				y: 56.0,
+	            x: '2010',
+	            population:'American Indian or Alaska Native'
+	    			},
 
-    			{
-    				y: 65.8,
-            x: '2011',
-            population:'American Indian or Alaska Native'
-    			},
+	    			{
+	    				y: 65.8,
+	            x: '2011',
+	            population:'American Indian or Alaska Native'
+	    			},
 
-    			{
-    				y: 69.6,
-            x: '2012',
-            population:'Hispanic or Latino'
-    			}
-    		]
-    	}
-    ]
-	}, 
+	    			{
+	    				y: 69.6,
+	            x: '2012',
+	            population:'Hispanic or Latino'
+	    			}
+	    		]
+	    	}
+	    ]
+		}/*, 
 
-	function(chart){
-		chart.renderer.path(['M', 0, 0, 'L', 100, 100, 200, 50, 300, 100])
-            .attr({
-                'stroke-width': 2,
-                stroke: 'red'
-            })
-            .add();
+		this will create a random line, but will need access to chart object 
+		OR
+		do another graph column
 
 
-	});
+		function(chart){
+			chart.renderer.path(['M', 200, 30, 'L', 100, 100]).attr({'stroke-width': 2, stroke: 'blue'}).add();
+		}*/
+	);
 
 	//and now some jQuerying about
 	var dataSetBest = chart.options.series[0].data;
