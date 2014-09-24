c6e721e230ee75e9bad92d5d816d16db2a6be430
Joseph Bacal <bacalj@me.com>
2014-09-24 16:24:13 -0400

    color change on columns

--- js/details.js
+++ js/details.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 			panning: true
 		},
 
-		colors: ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce', 
-   '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'],
+	colors: ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', 
+   '#92A8CD' , '#A47D7C', '#B5CA92'],
 
 		tooltip: {
 			formatter: function(){
